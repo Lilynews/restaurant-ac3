aaf0755dd193e00fecdb95f06f10a3a685ca33f8
Lilynews <lilinews2021@gmail.com>
2022-12-21 16:50:07 -0500

    feat:setting mongoose

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-// setting variable, file
-const express = require('express')
+//////// setting variable, file, 3rd-party-tool
+const express = require('express') //install express
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')// install mongoose
 
-// setting routes
+
+//////// setting for DB connection 
+// only for non-production environment
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+// connect to mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true }) 
+
+// DB connected status
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
+//////// setting routes
 app.get('/', (req, res) => {
   res.send('yooo~~ A7 HW')
 })
 
-// setting listening
+//////// setting listening
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
