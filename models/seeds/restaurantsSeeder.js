5c4e6f85b114e292463ad855914b66c04ce1d2dd
Lilynews <lilinews2021@gmail.com>
2022-12-21 17:12:17 -0500

    feat:add restaurants seeder

---
+++ models/seeds/restaurantsSeeder.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurants') // refer restaurants model
+const restaurantList = require('../../restaurant.json').results
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  Restaurant.create(restaurantList)
+  console.log('mongodb connected!')
+})
