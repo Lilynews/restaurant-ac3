7a97303bcd61520d2210b3022ad92c9cc8b77219
Lilynews <lilinews2021@gmail.com>
2022-12-21 21:43:29 -0500

    feat: edit restaurant schema

--- models/restaurants.js
+++ models/restaurants.js
@@ -2,13 +2,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
+  id: {
+    type: String
+  },
   name: {
     type: String,
     required: true
   },
   name_en: {
-    type: String,
-    required: true
+    type: String
   },
   category: {
     type: String,
