e864fc46c9c2d47df79b6b35daaf2f027cd4a24c
Lilynews <lilinews2021@gmail.com>
2022-12-22 21:06:43 -0500

    feat: create sort function

--- app.js
+++ app.js
@@ -27,20 +27,113 @@ db.once('open', () => {
 })
 
 //////// setting express-handlebars
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: {
+    selected: (sortSelected) => {
+      if (sortSelected) return "selected"
+    }
+  }
+}))
 app.set('view engine', 'hbs')
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
+/////// use public static files
+app.use(express.static('public'))
 
 //////// setting routes
 // Home page
+// app.get('/', (req, res) => {
+//   RestaurantList.find() // data from restaurant model
+//     .lean() // data transfer to plain javascript object
+//     .then(restaurants => res.render('index', { restaurants })) // send data to index and render
+//     .catch(error => console.error(error)) // error handling
+// })
+// ===================================
+// app.get('/', (req, res) => {
+//   const sort = req.query.sort ? req.query.sort : { _id: 'asc' }
+//   const sortMapping = {
+//     'AtoZ': { name: 'asc' },
+//     'ZtoA': { name: 'desc' },
+//     'category': { category: 'asc' },
+//     'location': { location: 'asc' }
+//   }
+//   RestaurantList.find()
+//     .lean()
+//     .sort(sortMapping[sort])
+//     .then(restaurants => res.render('index', { restaurants }))
+//     .catch(err => console.log(err))
+// })
+
 app.get('/', (req, res) => {
-  RestaurantList.find() // data from restaurant model
-    .lean() // data transfer to plain javascript object
-    .then(restaurants => res.render('index', { restaurants })) // send data to index and render
-    .catch(error => console.error(error)) // error handling
+  const sort = req.query.sort || 'default'
+  const sortBy = {
+    default: { _id: 'asc' },
+    AtoZ: { name: 'asc' },
+    ZtoA: { name: 'desc' },
+    category: { category: 'asc' },
+    location: { location: 'asc' }
+  }
+  const sortSelected = { [sort]: true }
+
+  RestaurantList.find()
+    .lean()
+    .sort(sortBy[sort])
+    .then(restaurants => res.render('index', { restaurants, sortSelected }))
+    .catch(error => console.log(error))
 })
+
+//search function
+// app.get("/search", (req, res) => {
+//   if (!req.query.keyword) {
+//     res.redirect("/")
+//   }
+//   const keyword = req.query.keyword.trim().toLowerCase()
+//   RestaurantList.find({})
+//     .lean()
+//     .then(restaurants => {
+//       const filterRestaurantsData = restaurants.filter(
+//         restaurant =>
+//           restaurant.name.toLowerCase().includes(keyword) ||
+//           restaurant.category.includes(keyword)
+//       )
+//       res.render("index", { restaurants: filterRestaurantsData, keyword })
+//     })
+//     .catch(err => console.log(err))
+// })
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim()
+  const sort = req.query.sort || 'default'
+  const sortBy = {
+    default: { _id: 'asc' },
+    AtoZ: { name: 'asc' },
+    ZtoA: { name: 'desc' },
+    category: { category: 'asc' },
+    location: { location: 'asc' }
+  }
+  const sortSelected = { [sort]: true }
+
+  RestaurantList.find()
+    .lean()
+    .sort(sortBy[sort])
+    .then(restaurants => {
+      const searchResult = restaurants.filter(restaurant => {
+        return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
+      })
+      res.render("index", { restaurants: searchResult, keyword })
+    })
+    .catch(err => console.log(err))
+
+  // if (!searchResult.length) {
+  //   res.render('not-found', { keyword })
+  // } else {
+  //   res.render('index', { restaurants: searchResult, keyword, sortSelected })
+  // }
+})
+
 // new page
 app.get('/restaurants/new', (req, res) => {
   res.render('new')
@@ -63,11 +156,11 @@ app.get('/restaurants/:restaurant_id/edit', (req, res) => {
 app.post('/restaurants/:restaurant_id/edit', (req, res) => {
   const id = req.params.restaurant_id
   return RestaurantList.findByIdAndUpdate(id, req.body)
-  // return RestaurantList.findById(id)
-  //   .then(restaurant => {
-  //     restaurant = { name, name_en, category, image, location, phone, google_map, rating, description }
-  //     restaurant.save()
-  //   })
+    // return RestaurantList.findById(id)
+    //   .then(restaurant => {
+    //     restaurant = { name, name_en, category, image, location, phone, google_map, rating, description }
+    //     restaurant.save()
+    //   })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
@@ -93,15 +186,15 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
     .catch(error => console.log(error))
 })
 //search function
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim()
-  const sort = req.query.sort
-  const regex = new RegExp(keyword, 'i')
-  RestaurantList.find({ $or: [{ name: { $regex: regex } }, { category: { $regex: regex } }] })
-    .lean()
-    .sort(sort)
-    .then(filteredRestaurants => res.render('index', { restaurants: filteredRestaurants, keyword, sort }))
-})
+// app.get('/search', (req, res) => {
+//   const keyword = req.query.keyword.trim()
+//   const sort = req.query.sort
+//   const regex = new RegExp(keyword, 'i')
+//   RestaurantList.find({ $or: [{ name: { $regex: regex } }, { category: { $regex: regex } }] })
+//     .lean()
+//     .sort(sort)
+//     .then(filteredRestaurants => res.render('index', { restaurants: filteredRestaurants, keyword, sort }))
+// })
 
 
 
@@ -109,8 +202,7 @@ app.get('/search', (req, res) => {
 
 
 
-/////// use public static files
-app.use(express.static('public'))
+
 
 //////// setting listening
 app.listen(port, () => {
