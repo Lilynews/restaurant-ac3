7b970c5cf5e77c481fd27115d20242a94affd428
Lilynews <lilinews2021@gmail.com>
2023-02-09 21:15:11 -0500

    feat: add FB login function

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 // 引入 middleware
 const { authenticator } = require('../middleware/auth')
 
 router.use('/restaurants', authenticator, restaurants)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
