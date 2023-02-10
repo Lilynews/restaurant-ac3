88aff9aaa68a44c043f6193553342145b4188e51
Lilynews <lilinews2021@gmail.com>
2023-02-09 21:15:27 -0500

    feat: organized files

--- models/seeds/restaurantsSeeder.js
+++ models/seeds/restaurantsSeeder.js
@@ -1,8 +1,8 @@
 const Restaurant = require('../restaurants') // refer restaurants model
-const restaurantList = require('../../restaurant.json').results
+const restaurantList = require('./restaurant.json').results
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
   Restaurant.create(restaurantList)
-  console.log('mongodb connected!')
+  console.log('Restaurant created!')
 })
