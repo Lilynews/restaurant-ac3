7447ade56281efeaa4283306346ae154d7c924d2
Lilynews <lilinews2021@gmail.com>
2023-02-10 15:26:55 -0500

    feat: add login warning message

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.use((req, res, next) => {
   // middleware for flash
   res.locals.success_msg = req.flash('success_msg')  
   res.locals.warning_msg = req.flash('warning_msg') 
+  res.locals.error = req.flash('error')
 
   next()
 })
